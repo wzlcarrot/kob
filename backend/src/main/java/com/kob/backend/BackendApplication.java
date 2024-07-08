5346af0345f08c7b73714ee8d4df33624eb7ba84
acwer <1017949414@qq.com>
2024-07-09 00:05:47 +0800

    项目骨架已经搭建完成

--- backend/src/main/java/com/kob/backend/BackendApplication.java
+++ backend/src/main/java/com/kob/backend/BackendApplication.java
@@ -3,6 +3,8 @@ package com.kob.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
+
 @SpringBootApplication
 public class BackendApplication {
 
